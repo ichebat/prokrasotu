6c0c913abaf5f600d4d63191efd8ef8761326013
ichebat <ichebat@gmail.com>
2024-06-26 11:47:41 +0500

    Add TelegramService

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TelegramService } from '../../services/telegram.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html'
 })
 export class ShopComponent {
+  telegram = inject(TelegramService);
+
+  /**
+   *
+   */
+  constructor() {
+    this.telegram.MainButton.show();
+    
+  }
 
 }
