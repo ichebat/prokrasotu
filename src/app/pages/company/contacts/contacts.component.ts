90ed610030206f92f74c0d3671308b68b5533348
ichebat <ichebat@gmail.com>
2024-09-18 16:49:46 +0500

    ng update

--- src/app/pages/company/contacts/contacts.component.ts
+++ src/app/pages/company/contacts/contacts.component.ts
@@ -2,31 +2,30 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationService } from '../../../services/navigation.service';
 import { TelegramService } from '../../../services/telegram.service';
 import { environment } from '../../../../environments/environment';
+import { QRCodeComponent } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrl: './contacts.component.scss',
 })
-export class ContactsComponent  implements OnInit, OnDestroy{
-
+export class ContactsComponent implements OnInit, OnDestroy {
   constructor(
     public telegramService: TelegramService,
-    private navigation: NavigationService,){
-
-      this.goBack = this.goBack.bind(this);
-
+    private navigation: NavigationService,
+  ) {
+    this.goBack = this.goBack.bind(this);
   }
 
   ngOnInit(): void {
-    if (this.telegramService.IsTelegramWebAppOpened){      
+    if (this.telegramService.IsTelegramWebAppOpened) {
       this.telegramService.BackButton.show();
       this.telegramService.BackButton.onClick(this.goBack); //при передаче параметра this теряется, поэтому забандить его в конструкторе
     }
   }
 
   ngOnDestroy(): void {
-    if (this.telegramService.IsTelegramWebAppOpened){      
+    if (this.telegramService.IsTelegramWebAppOpened) {
       this.telegramService.BackButton.hide();
       this.telegramService.BackButton.offClick(this.goBack);
     }
@@ -36,8 +35,7 @@ export class ContactsComponent  implements OnInit, OnDestroy{
     this.navigation.back();
   }
 
-  getUrlForTelegram(){
-    
-      return environment.webAppDirectLink;
+  getUrlForTelegram() {
+    return environment.webAppDirectLink;
   }
 }
