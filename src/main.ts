cdbf4664a7964124efec638ccf87ecc10996e223
ichebat <ichebat@gmail.com>
2024-09-19 11:16:05 +0500

    Auto-generated commit

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-//config(); // load environment variables from .env file
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
