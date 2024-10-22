fa446a3b7c3440e418ac77e28f23724250638295
Ivan Che <chebatz@mail.ru>
2024-10-22 22:20:57 +0500

    little changes

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -280,7 +280,7 @@ export class ProductItemComponent implements OnInit, OnDestroy {
         this.product?.imageUrl,
         [
           Validators.minLength(2),
-          Validators.maxLength(255),
+          Validators.maxLength(1000),
           Validators.pattern(
             '^(http://www.|https://www.|http://|https://)?[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$',
           ),
